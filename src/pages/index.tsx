442f6e3ee85c5b3e8fa308aa6e4a722234d1d787
edz <guohaotian@hecom.cn>
2020-12-25 15:30:06 +0800

    升级到react17

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './index.less';
 
 export default () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // 使用浏览器的 API 更新页面标题
+    document.title = `You clicked ${count} times`;
+  });
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
-}
+};
